3c0c885782ad447ab424bd294279d5e6b4b686d5
DESKTOP-C2OUG3H\MhdSyd014 <irsyadmuhammad954@gmail.com>
2024-06-23 22:37:48 +0700

    Navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-import "flowbite";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <nav className="bg-blue-950 border-gray-200">
@@ -19,7 +24,8 @@ const Navbar = () => {
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:bg-blue-800 dark:focus:ring-blue-800"
             aria-controls="navbar-default"
-            aria-expanded="false"
+            aria-expanded={isOpen ? "true" : "false"}
+            onClick={handleToggle}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -38,7 +44,12 @@ const Navbar = () => {
               />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div
+            className={`${
+              isOpen ? "block" : "hidden"
+            } w-full md:block md:w-auto`}
+            id="navbar-default"
+          >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-blue-950 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li>
                 <a
