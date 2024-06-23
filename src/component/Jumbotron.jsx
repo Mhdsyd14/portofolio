8fb57adddbe10cfd33aabb301f16becd8dbc7278
DESKTOP-C2OUG3H\MhdSyd014 <irsyadmuhammad954@gmail.com>
2024-06-23 18:05:42 +0700

    Ubah lagi hehe

--- src/component/Jumbotron.jsx
+++ src/component/Jumbotron.jsx
@@ -10,26 +10,26 @@ const Jumbotron = () => {
   });
 
   return (
-    <section className="bg-blue-950  flex flex-col md:flex-row md:h-[480px] md: w-full max-[363px]: h-max  ">
-      <div className="font-sans py-5 px-4 mt-2 md:mt-[110px] mx-4 md:mx-12 text-left w-[630px] max-[363px]: ml-0 text-3xl  ">
-        <h1 className="mb-4 font-semibold tracking-tight text-white  md:text-5xl max-[563px]: text-3xl">
-          I am <span className="text-amber-400">Muhammad Irsyad</span>
-        </h1>
-        <h2 className="text-2xl md:text-3xl font-semibold text-white mt-1">
-          <span className="text-amber-400">{text}</span> Developer
-        </h2>
-        <div className="flex flex-row mt-5 text-3xl md:text-4xl gap-5 text-white">
-          <FaWhatsapp className="hover:text-amber-400" />
-          <FaGithub className="hover:text-amber-400" />
-          <FaFileDownload className="hover:text-amber-400" />
+    <section className="bg-blue-950 h-screen">
+      <div className=" flex flex-col font-sans py-5 px-4 mx-4 text-left lg:flex-row">
+        <div className="flex flex-col justify-center lg: w-7/12">
+          <h1 className=" w-[450px] font-semibold tracking-tight text-white text-3xl mt-3 lg:text-5xl lg:w-11/12">
+            I am <span className="text-amber-400">Muhammad Irsyad</span>
+          </h1>
+
+          <h2 className="md:text-3xl font-semibold text-white mt-1 max-[563px]: text-lg">
+            <span className="text-amber-400"> {text}</span> Developer
+          </h2>
+          <div className="flex flex-row mt-5 text-3xl md:text-4xl gap-5 text-white">
+            <FaWhatsapp className="hover:text-amber-400" />
+            <FaGithub className="hover:text-amber-400" />
+            <FaFileDownload className="hover:text-amber-400" />
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center items-center   ">
-        <img
-          className="md:mt-1 mb-8 md:ml-[90px]  md:w-[450px] lg:ml-[80px]   "
-          src={Profil}
-          alt="Profile"
-        />
+
+        <div className="flex justify-center items-center  ml-4 mt-[25px] lg:ml-[60px]">
+          <img className=" w-[480px] " src={Profil} alt="Profile" />
+        </div>
       </div>
     </section>
   );
